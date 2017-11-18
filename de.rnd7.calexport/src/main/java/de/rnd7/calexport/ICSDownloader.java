efe4b486cbfe079a583710bd401ee663ab7804e4
Philipp Arndt <2f.mail@gmx.de>
2017-11-18 08:02:19 +0100

    added ignore for test files and fixed resource issue

--- de.rnd7.calexport/src/main/java/de/rnd7/calexport/ICSDownloader.java
+++ de.rnd7.calexport/src/main/java/de/rnd7/calexport/ICSDownloader.java
@@ -20,26 +20,28 @@ public class ICSDownloader {
 	public static InputStream download(final String surl) throws Exception {
 		final String replaced = surl.replace("webcal://", "https://");
 		final HttpGet httpget = new HttpGet(replaced);
-		final CloseableHttpClient httpclient = HttpClients.createDefault();
 
-		// Create a custom response handler
-		final ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+		try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
+			// Create a custom response handler
+			final ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 
-			@Override
-			public String handleResponse(
-					final HttpResponse response) throws ClientProtocolException, IOException {
-				final int status = response.getStatusLine().getStatusCode();
-				if (status >= 200 && status < 300) {
-					final HttpEntity entity = response.getEntity();
-					return entity != null ? EntityUtils.toString(entity) : null;
-				} else {
-					throw new ClientProtocolException("Unexpected response status: " + status);
+				@Override
+				public String handleResponse(
+						final HttpResponse response) throws ClientProtocolException, IOException {
+					final int status = response.getStatusLine().getStatusCode();
+					if (status >= 200 && status < 300) {
+						final HttpEntity entity = response.getEntity();
+						return entity != null ? EntityUtils.toString(entity) : null;
+					} else {
+						throw new ClientProtocolException("Unexpected response status: " + status);
+					}
 				}
-			}
 
-		};
+			};
 
-		final String ics = httpclient.execute(httpget, responseHandler);
-		return new ByteArrayInputStream(ics.getBytes("utf-8"));
+			final String ics = httpclient.execute(httpget, responseHandler);
+			return new ByteArrayInputStream(ics.getBytes("utf-8"));
+		}
+
 	}
 }
