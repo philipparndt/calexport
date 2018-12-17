9135e490fd0bb54d82c35b99da573fe5de66d45e
Philipp Arndt <2f.mail@gmx.de>
2018-12-17 14:32:35 +0100

    Added html to pdf converter based on electron

---
+++ de.rnd7.calexport.pdf/src/main.ts
@@ -0,0 +1,44 @@
+import { app } from "electron";
+
+import * as fs from 'fs';
+import * as pdf from './pdfrenderer';
+
+app.disableHardwareAcceleration();
+
+console.log(`Welcome to PDF converter.
+This will convert all *.html files in a given folder to single page PDF files.`);
+
+const targetFolder = target(process.argv);
+if (!targetFolder) {
+	console.log("please specify a target folder");
+	app.quit();
+}
+else {
+	console.log(targetFolder);
+
+	app.once('ready', () => {
+		const dir = fs.readdirSync(targetFolder);
+	
+		var promises = dir.filter(element => element.endsWith('.html'))
+		.map(element => `${targetFolder}/${element}`)
+		.map(file => pdf.render(file));
+		
+		Promise.all(promises).then(() => {
+			console.log("==== Completed all ====");
+			app.quit();
+		});
+	});
+}
+
+function target(args: string[]) : string {
+	if(args) {
+		if (args.length == 3 && args[1].includes('main.js')) {
+			// executed using npm start
+			return args[2];	
+		}
+		else if (args.length == 2 && !args[1].includes('main.js')) {
+			// executed the exported app
+			return args[1];	
+		}
+	}
+}
---
+++ de.rnd7.calexport.pdf/src/pdfrenderer.ts
@@ -0,0 +1,100 @@
+import { BrowserWindow, PrintToPDFOptions, WebContents, app } from "electron";
+const pdf = require('pdf-parse');
+import * as fs from 'fs';
+
+export function render(sourceFile: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        savePDF(sourceFile).then(resolve);
+    });
+}
+
+async function savePDF(sourceFile: string) {
+    let window = await loadDocument(sourceFile);
+    let webContents = window.webContents;
+
+    var data: Buffer;
+
+    for (var zoomLevel = 100; zoomLevel > 70; zoomLevel-=2.5) {
+        webContents.insertCSS(`@media print {html, body {zoom: ${zoomLevel}%;}`);
+
+        console.log(`Generating page with zoomLevel: ${zoomLevel}%`);
+        data = await printToPDF(webContents);
+    
+        let pageCount = await pages(data);
+
+        if (pageCount == 1) {
+            break;
+        }
+        else {
+            console.log(`too many pages: ${pageCount}`);
+        }
+    }
+
+    let targetFile = `${sourceFile}.pdf`;
+    await writeFile(targetFile, data);
+    console.log(`pdf file written: ${targetFile}`)
+}
+
+function loadDocument(sourceHtml: string): Promise<BrowserWindow> {
+    return new Promise<BrowserWindow>((resolve, reject) => {
+        let window = new BrowserWindow({
+            show: false,
+            webPreferences: {
+                offscreen: true
+            }
+        });
+        
+        window.loadFile(sourceHtml);
+        window.webContents.on("did-finish-load", () => {
+            resolve(window);
+        });
+    });        
+}
+
+function options(): PrintToPDFOptions {
+    return {
+        marginsType: 0,
+        pageSize: 'A4',
+        printBackground: true,
+        printSelectionOnly: false,
+        landscape: false
+    }
+}
+
+function printToPDF(webContents: WebContents): Promise<Buffer> {
+    return new Promise<Buffer>((resolve, reject) => {
+        webContents.printToPDF(options(), (error, data) => {
+            if (error) {
+                reject(error);    
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });        
+}
+
+const pages = (data: Buffer) => pdf(data).then((document: any) => {
+    return document.numpages;
+});
+
+function writeFile(fileName: string, data: Buffer): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        fs.writeFile(fileName, data, (error) => {
+            if (error) {
+                reject(error);    
+            }
+            else {
+                resolve();
+            }
+        });
+    });        
+}
+
+function documentWritten(error: Error) {
+    if (error) {
+        throw error
+    }
+
+    console.log('Write PDF successfully.')
+}
