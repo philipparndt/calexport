56be830a75799076875f8cb63827d1c9b56a44f9
Philipp Arndt <2f.mail@gmx.de>
2019-09-13 16:52:07 +0200

    fix tslint warnings

--- de.rnd7.calexport.pdf/src/pdfrenderer.ts
+++ de.rnd7.calexport.pdf/src/pdfrenderer.ts
@@ -1,76 +1,75 @@
-import * as electron from 'electron';
-import * as fs from 'fs';
+import * as electron from "electron";
+import * as fs from "fs";
 
-const pdf = require('pdf-parse');
+const pdf = require("pdf-parse");
 
 export function render(sourceFile: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => 
+    return new Promise<void>((resolve, reject) =>
         savePDF(sourceFile)
         .then(resolve)
-        .catch(error => reject(error)));
+        .catch((error) => reject(error)));
 }
 
 async function savePDF(sourceFile: string) {
     const window = await loadDocument(sourceFile);
     const webContents = window.webContents;
 
-    var data: Buffer;
-    var zoomLevel = 100;
+    let data: Buffer;
+    let zoomLevel = 100;
 
-    for (; zoomLevel > 70; zoomLevel-=2.5) {
+    for (; zoomLevel > 70; zoomLevel -= 2.5) {
         webContents.insertCSS(`@media print {html, body {zoom: ${zoomLevel}%;}`);
 
         data = await printToPDF(webContents);
-    
+
         const pageCount = await pdf(data)
         .then((document: any) => document.numpages)
         .catch((error: any) => console.log(`Error parsing pdf for file '${sourceFile}': ${error}`));
 
-        if (pageCount == 1) {
+        if (pageCount === 1) {
             break;
-        }
-        else {
+        } else {
             console.log(`too many pages: ${pageCount} with zoomLevel: ${zoomLevel}%`);
         }
     }
 
-    const targetFile = sourceFile.replace(/\.html$/ig, '.pdf');
+    const targetFile = sourceFile.replace(/\.html$/ig, ".pdf");
     await writeFile(targetFile, data);
-    console.log(`pdf file written: ${targetFile} (${zoomLevel}% scale)`)
+    console.log(`pdf file written: ${targetFile} (${zoomLevel}% scale)`);
 }
 
 function loadDocument(sourceHtml: string): Promise<electron.BrowserWindow> {
     return new Promise<electron.BrowserWindow>((resolve, reject) => {
-        let window = new electron.BrowserWindow({
+        const window = new electron.BrowserWindow({
             show: false,
             webPreferences: {
-                offscreen: true
-            }
+                offscreen: true,
+            },
         });
-        
+
         window.loadFile(sourceHtml);
         window.webContents.on("did-finish-load", () => {
             resolve(window);
         });
-    });        
+    });
 }
 
 function options(): electron.PrintToPDFOptions {
     return {
+        landscape: false,
         marginsType: 0,
-        pageSize: 'A4',
+        pageSize: "A4",
         printBackground: true,
         printSelectionOnly: false,
-        landscape: false
-    }
+    };
 }
 
 function printToPDF(webContents: electron.WebContents): Promise<Buffer> {
-    return new Promise<Buffer>((resolve, reject) => 
-        webContents.printToPDF(options(), (error, data) => error ? reject(error) : resolve(data)));        
+    return new Promise<Buffer>((resolve, reject) =>
+        webContents.printToPDF(options(), (error, data) => error ? reject(error) : resolve(data)));
 }
 
 function writeFile(fileName: string, data: Buffer): Promise<void> {
-    return new Promise<void>((resolve, reject) => 
-        fs.writeFile(fileName, data, (error) => error ? reject(error) : resolve()));        
+    return new Promise<void>((resolve, reject) =>
+        fs.writeFile(fileName, data, (error) => error ? reject(error) : resolve()));
 }
